71997cfa3e51e66e9da8d89bf5b3efa6556ce4d6
Sergey-grishko <33630597+Sergey-grishko@users.noreply.github.com>
2018-11-02 16:30:43 +0200

    Add files via upload

---
+++ precache-manifest.1d66e713eed579590610ad69b76bba9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d6b5ce5f321479171cfb",
+    "url": "/static/js/main.d6b5ce5f.chunk.js"
+  },
+  {
+    "revision": "24fc8bb7dc12f50487b0",
+    "url": "/static/js/1.24fc8bb7.chunk.js"
+  },
+  {
+    "revision": "d6b5ce5f321479171cfb",
+    "url": "/static/css/main.52e90552.chunk.css"
+  },
+  {
+    "revision": "b2e01c4b1697470567ef7388db0a2fb7",
+    "url": "/index.html"
+  }
+];
